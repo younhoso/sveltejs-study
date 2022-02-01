42d0d288f16fa4a1ffd118d59ea08d7204482d2a
YounHoSo <mac@Macui-iMac.local>
2022-02-01 11:32:24 +0900

    스벨트 세팅 및 라우터 설정

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sveltejs-study
+
+### 필요한 라이브러리 설치하기 
+```
+npx degit sveltejs/template svelte-app
+npm installl svelte-routing
+```
---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	import About from "./routes/About.svelte";
+
+	export let url = "";
+</script>
+
+<Router url="{url}">
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+	</nav>
+	<div>
+		<Route path="/"><Home /></Route>
+		<Route path="about" component={About} />
+	</div>
+</Router>
+
+<style>
+	
+</style>
